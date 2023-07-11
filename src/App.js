823835d7e5d47e927365ead665eb51a1a0d04d1f
Shawn Lentz <shawn.lentz@gmail.com>
2023-07-11 11:38:38 -1000

    changed redrect uri to gh pages url

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import '../src/styles/components.css'
 import Login from './Login'
 import Dashboard from '../src/components/Dashboard'
 
-const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=a4a7e54594ce47ef93cc7eef30966349&response_type=code&redirect_uri=http://localhost:3001&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20ugc-image-upload%20playlist-modify-private%20playlist-modify-public%20user-read-recently-played%20user-follow-read'
+// http://localhost:3001 for dev
+const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=a4a7e54594ce47ef93cc7eef30966349&response_type=code&redirect_uri=https://shaawwn.github.io/spotify-playlists/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20ugc-image-upload%20playlist-modify-private%20playlist-modify-public%20user-read-recently-played%20user-follow-read'
 
 
 const code = new URLSearchParams(window.location.search).get('code')
@@ -15,7 +16,9 @@ function App() {
 
   useEffect(() => {
     console.log("Loading app")
+    // initialize the player here?
   }, [])
+
   return code ? <Dashboard code={code} />: <Login authUrl={AUTH_URL}/>
 }
 
