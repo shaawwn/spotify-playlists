487f0db0a6bb9dcfb01dbbb5353c9faa315082ff
Shawn Lentz <shawn.lentz@gmail.com>
2023-08-15 15:39:13 -1000

    changed track-table-row to more closely resemble spotify app

--- src/components/TrackTableRow.js
+++ src/components/TrackTableRow.js
@@ -1,6 +1,8 @@
 import {useState, useEffect} from 'react';
 import {msToMinutesAndSeconds, truncateText, truncateTextLong} from '../utils/formatting'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faTrash} from '@fortawesome/free-solid-svg-icons'
 
 function TrackTableRow({item, removeTrack, play, pause, context, offset}) {
     // console.log("ROW ITEM", item)
@@ -13,31 +15,26 @@ function TrackTableRow({item, removeTrack, play, pause, context, offset}) {
     }
 
     function handleClick(e) { // uri and context
-        // e.stopPropagation()
-        // console.log("item", context.uri, offset) 
-        // console.log(e.target, item)
-        // console.log("Clicking", uri, name)
+
         const id = document.getElementById(item.id)
-        // id.classList.add('track-table-row-active')
-        console.log(id)
         id.classList.add('track-table-row-active')
         play(null, context.uri, offset)
-        // if(active === true) {
-        //     setActive(false)
-        // } else if(active === false) {
-        //     setActive(true)
-        // }
     }
     
     function displayRow() {
         return(
             <div className="track-table-row" id={item.id} onClick={(e) => handleClick(e)}>
-                <div className="track-table-cell-name">{truncateTextLong(item.name)}</div>
-                <div className="track-table-cell">{item.artists[0].name}</div>
-                <div className="track-table-cell">{truncateText(item.album.name)}</div>
-                <div className="track-table-cell">{msToMinutesAndSeconds(item.duration_ms)}</div>
                 <div className="track-table-cell">
-                    <button className="search-row-btn" onClick={(e) => handleButtonClick(e, item.id)}>Remove</button>
+                    <div className="track-table-details">
+                        <p className="track-table-details-song">{truncateTextLong(item.name)}</p>
+                        <p className="track-table-details-artist">{item.artists[0].name}</p>
+                    </div>
+                </div>
+
+                <div className="track-table-cell flex-end">
+                    <p style={{'margin-top': 'auto', 'margin-bottom': 'auto'}}>{msToMinutesAndSeconds(item.duration_ms)}</p>
+                    <FontAwesomeIcon    className="track-table-delete" icon={faTrash} 
+                    />
                 </div>
             </div>
         )
